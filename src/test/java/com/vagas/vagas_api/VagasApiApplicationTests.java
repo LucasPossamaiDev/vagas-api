996c0b6eca703695ed6b8896dbd5c314327e19b4
LucasPossamaiDev <lucaspossamaidev@gmail.com>
2026-09-01 23:17:26 -0300

    Setup inicial do projeto vagas-api

---
+++ src/test/java/com/vagas/vagas_api/VagasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagas.vagas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
